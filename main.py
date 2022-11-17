7fd243e240dec128f74ff6c16d0eac83edea57c9
nosnic <nosilkin.n.d@gmail.com>
2022-11-17 17:26:13 +0300

    сделяль апи

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from server.app import app
 from server.handlers.UserApi import UserApi
+from server.handlers.AricleApi import ArticleApi
+from server.handlers.CommentApi import CommentApi
 
 
 API_URL = '/api/v1.0'
@@ -7,6 +9,8 @@ API_URL = '/api/v1.0'
 
 def main():
     app.add_url_rule(f'{API_URL}/users/', view_func=UserApi.as_view('users'))
+    app.add_url_rule(f'{API_URL}/articles/', view_func=ArticleApi.as_view('articles'))
+    app.add_url_rule(f'{API_URL}/comments/', view_func=CommentApi.as_view('comments'))
     app.run()
 
 
